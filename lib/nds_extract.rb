a6240211c3a65ccb83d3ef0d366c4056e36c39fa
Finn Thye <piqueen@gmail.com>
2019-11-10 08:58:44 -0700

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,21 +2,53 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+
+  row_index = 0
+  while row_index < nds.length do
+    director = nds[row_index][:name]
+    movies = nds[row_index][:movies] 
+    movie_index = 0
+    revenue_all = 0
+
+    while movie_index < movies.length do
+      revenue_current = movies[movie_index][:worldwide_gross]
+      revenue_all += revenue_current
+      movie_index += 1
+    end
+    result[director] = revenue_all
+
+    row_index += 1
+  end
+
+  result
 end
+
+# {:name=>"Quentin Tarantino",
+#   :movies=>
+#    [{:title=>"Django Unchained",
+#      :studio=>"Weinstein",
+#      :worldwide_gross=>162805434,
+#      :release_year=>2012},
+#     {:title=>"Once Upon a Time in Hollywood",
+#      :studio=>"Sony",
+#      :worldwide_gross=>135156125,
+#      :release_year=>2019},
+#     {:title=>"Inglourious Basterds",
+#      :studio=>"Weinstein",
+#      :worldwide_gross=>120540719,
+#      :release_year=>2009},
+#     {:title=>"Pulp Fiction",
+#      :studio=>"Miramax",
+#      :worldwide_gross=>107928762,
+#      :release_year=>1994},
+#     {:title=>"Kill Bill Vol. 1",
+#      :studio=>"Miramax",
+#      :worldwide_gross=>70099045,
+#      :release_year=>2003},
+#     {:title=>"Kill Bill Vol. 1",
+#      :studio=>"Miramax",
+#      :worldwide_gross=>66208183,
+#      :release_year=>2004}]},
